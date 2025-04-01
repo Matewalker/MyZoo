083241add6daebcac3e784d0617e5f3ebb59ac2f
Matewalker <148473036+Matewalker@users.noreply.github.com>
2025-04-01 10:38:06 +0200

    Add files via upload

--- MyZoo.Server/Models/UserModel.cs
+++ MyZoo.Server/Models/UserModel.cs
@@ -6,14 +6,16 @@ namespace MyZoo.Server.Models
     {
         public int Id { get; set; }
         [Required]
+        [MaxLength(50)]
         public string Username { get; set; }
         [Required]
+        [MaxLength(100)]
         public string PasswordHash { get; set; }
-        public int Capital { get; set; }
+        public int Capital { get; set; } = 5000;
         public string? WarehouseAnimals { get; set; }
         public string? ZooAnimals { get; set; }
-        public int TicketPrices { get; set; }
+        public int TicketPrices { get; set; } = 0;
         public DateTime CurrentDate { get; set; } = new DateTime(2025, 1, 1);
-        public int Visitors { get; set; }
+        public int Visitors { get; set; } = 0;
     }
 }
