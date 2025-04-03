1be42f57080b1bb9fff838352fb380cea71cc67c
Matewalker <148473036+Matewalker@users.noreply.github.com>
2025-04-03 09:27:12 +0200

    Add files via upload

--- MyZoo.Server/Models/AnimalSpecies.cs
+++ MyZoo.Server/Models/AnimalSpecies.cs
@@ -4,7 +4,6 @@
     {
         public int Id { get; set; }
         public string Species { get; set; }
-        public ICollection<Animals> Animals { get; set; }
         public ICollection<AnimalContinents> AnimalContinents { get; set; }
         public ICollection<AnimalFeeds> AnimalFeeds { get; set; }
     }
